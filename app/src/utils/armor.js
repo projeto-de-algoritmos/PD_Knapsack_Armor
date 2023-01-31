7d03342f9560557da49493e5db6e015595af603b
Guilherme Barbosa <mrguilhermeferreira@gmail.com>
2023-01-31 00:50:05 -0300

    feat: create initial json with armor data

---
+++ app/src/utils/armor.js
@@ -0,0 +1,50 @@
+export const armor = {
+  "items": {
+    "chestplate": [
+      {
+        "id": 1,
+        "name": "Peitoral de Couro",
+        "weigth": 15,
+        "protection": 3,
+        "image": require("../assets/peitoral_couro.png")
+      },
+      {
+        "id": 2,
+        "name": "Peitoral de Bronze",
+        "weigth": 18.5,
+        "protection": 4,
+        "image": require("../assets/peitoral_bronze.png")
+      },
+      {
+        "id": 3,
+        "name": "Peitoral de Ferro",
+        "weigth": 20,
+        "protection": 5,
+        "image": require("../assets/peitoral_ferro.png")
+      }
+    ],
+    "boots": [
+      {
+        "id": 1,
+        "name": "Botas Oficial Guerreiro",
+        "weigth": 2.5,
+        "protection": 1,
+        "image": require("../assets/botas_oficial_guerreiro.png")
+      },
+      {
+        "id": 2,
+        "name": "Botas Macabro Guerreiro",
+        "weigth": 4,
+        "protection": 3,
+        "image": require("../assets/botas_macabro_guerreiro.png")
+      },
+      {
+        "id": 3,
+        "name": "Botas Mestre Guerreiro",
+        "weigth": 5,
+        "protection": 4,
+        "image": require("../assets/botas_mestre_guerreiro.png")
+      }
+    ]
+  }
+}
